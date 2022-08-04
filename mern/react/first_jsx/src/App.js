44580c2a89138bc39ac1394129ea2f57f04f6d46
Melissa Curylo <melissalcurylo@gmail.com>
2022-08-04 14:04:21 -0400

    initial commit

---
+++ mern/react/first_jsx/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <ul class="ul_li">
+          <li>Learn react</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+          <li>Learng Git</li>
+        </ul>
+
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
