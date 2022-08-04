7bc35ddf39a13bbfddb888c22516dce6de0d8bd3
Melissa Curylo <melissalcurylo@gmail.com>
2022-08-04 14:02:55 -0400

    initial commit

---
+++ mern/react/axios_pokemon_api/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import PokemonApi from './views/PokemonApi';
+
+function App() {
+  return (
+    <div className="Container">
+
+      <PokemonApi/>
+
+    </div>
+  );
+}
+
+export default App;
