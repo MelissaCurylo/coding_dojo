0849f832f2946a84ba699de51b3eec4e2486ee16
Melissa Curylo <melissalcurylo@gmail.com>
2022-08-04 14:10:00 -0400

    initial commit

---
+++ java/java_foundations/first_java_program/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava{
+    public static void main(String[] args) {
+        System.out.println("My Name is Melissa");
+        System.out.println("I love Cats and Yoga");
+        System.out.println("My favorite book is called 'The Century of the Surgeon'");
+    }
+}
