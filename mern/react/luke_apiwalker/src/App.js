5d79079c452d6d043b35d534f6a5afc4f49a161a
Melissa Curylo <melissalcurylo@gmail.com>
2022-08-04 14:06:04 -0400

    initial commit

---
+++ mern/react/luke_apiwalker/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { Routes, Route, Link } from 'react-router-dom'
+import { ErrorPage } from './views/ErrorPage';
+import { People } from './views/People';
+import { Planet } from './views/Planet';
+import { Form } from './components/Form'
+
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <h1> Star Wars API</h1>
+      <Form/>
+      
+      <Routes>
+        <Route path="/people/:id" element={ <People /> } />
+        <Route path="/planet/:id" element={<Planet />}  />
+        <Route path="*" element={<ErrorPage/>} />
+      </Routes>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ mern/react/luke_apiwalker/src/views/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const ErrorPage = () => {
+
+    const ErrorPage = () =>{
+
+        
+        return{}}
+
+
+  return (
+    <div>
+
+
+    </div>
+  )
+}
