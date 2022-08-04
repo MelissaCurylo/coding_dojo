7faa02fd97029c5c7b9b727bede62847ed39fd62
Melissa Curylo <melissalcurylo@gmail.com>
2022-08-04 14:07:11 -0400

    initial commit

---
+++ mern/react/putting_it_together/src/components/PersonCardComponent.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+    
+    
+class PersonCardComponent extends Component {
+    constructor(props){     // declared constructor to override defaultand set state object
+        super(props);       // super gives all functionality that comes with Component
+        this.state = {      // state = change
+            age : props.age         // age changing here -- attributes for comparison
+        }
+    }
+
+    render(){
+        const { firstName, lastName, hairColor } = this.props;
+        const addAgeByOne = () => { this.setState({ age : this.state.age +1 }) } // state = change
+        return(
+            <div>
+                <div>
+                {/* <header class="data_header"> { dataHeader }</header> */}
+                </div>
+                <h1>{ firstName } { lastName }</h1> 
+                <ul class="list">
+                    <li> Age:  { this.state.age } </li>
+                    <li> Hair Color: { hairColor } </li>
+
+                    <button onClick = {() => addAgeByOne()} >  Add One to Age </button>
+
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default PersonCardComponent;
