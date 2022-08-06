2613f8c75e2a18878834002c5b351154e5846d7d
Melissa <95829904+MelissaCurylo@users.noreply.github.com>
2022-08-06 19:34:16 -0400

    Add files via upload

--- java/Spring/spring_push/SpringBoot/daikichi-path-variables/src/main/java/com/melissacurylo/daikichipathvar/controllers/DaikichiController.java
+++ java/Spring/spring_push/SpringBoot/daikichi-path-variables/src/main/java/com/melissacurylo/daikichipathvar/controllers/DaikichiController.java
@@ -1,25 +1,43 @@
 package com.melissacurylo.daikichipathvar.controllers;
 
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/daikichi")
 public class DaikichiController {
 	
-	@RequestMapping("")
+	@RequestMapping("/daikichi")
 	public String daikichi() {
 		return "Welcome";
 	}
 	
-	@RequestMapping("/today")
+	@RequestMapping("/daikichi/today")
 	public String today() {
 		return "Today you will find luck in all your endeavors!";
 	}
 	
-	@RequestMapping("/tomorrow")
+	@RequestMapping("/daikichi/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
+		
+	
+	@RequestMapping("/daikichi/travel/{city}")
+    public String travelTest(@PathVariable("city") String city){
+    	return "You will soon travel too"+ " "  + city;
+    }
+	
+	@RequestMapping("/daikichi/lotto/{guess}")
+    public String lottoTest(@PathVariable("guess") String guess){
+		int newNum = Integer.parseInt(guess);
+		
+		if ( newNum % 2 == 1 ){
+			return("You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.");			
+		}
+		return("You will take a grand journey in the near future, but be weary of tempting offers");
+    }
+
 
 }
